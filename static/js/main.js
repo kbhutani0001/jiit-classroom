ee6d93750516ffc696f771125aa87343219a2fa0
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-04-18 18:43:33 +0530

    major, changed all html to jinja templates

---
+++ static/js/main.js
@@ -0,0 +1,41 @@
+const generateClassroomNumber = () => {
+  var zoomId = parseInt(document.getElementById('zoomId').value)
+  document.getElementById('classroomId').value = zoomId + 620128
+  var classroomId = document.getElementById('classroomId').value
+  document.getElementById("classroomId").disabled = true;
+  document.getElementById('meetingUrl').value = `https://jiitclassroom.herokuapp.com/join/${classroomId}`
+  document.getElementById("meetingUrl").disabled = true;
+
+  document.getElementById('classroomIdDiv').style.display = 'block';
+}
+
+const copyToClipboard = (code) => {
+  if(code==1){
+    var str = document.getElementById('classroomId').value 
+  }
+  else{
+    var str = document.getElementById('meetingUrl').value 
+  }
+
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+
+
+};
+
+const redirectToClass = () => {
+  classroomNumber = document.getElementById('classroomNumber').value
+  window.location = '/join/' + classroomNumber
+}
+
+const validateSignup = () => {
+  if(document.getElementById('facultyPassword').value!=document.getElementById('facultyPassword2').value) {
+    alert('Please enter same password again')
+    event.preventDefault()
+
+  }
+}
