0cc82ef1238120e7e7c7a3a55a17753159a04cec
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-04-19 22:04:27 +0530

    added previous attendance check

--- static/js/main.js
+++ static/js/main.js
@@ -19,7 +19,7 @@ const copyToClipboard = (code) => {
 const redirectToClass = () => {
   classroomId = document.getElementById('classroomNumber').value
   if(classroomId.length >= 9){
-    window.location = '/join/' + classroomNumber
+    window.location = '/join/' + classroomId
   }
   else {
     window.alert("Please enter a valid Classroom ID")
